ada270cf0fe5e73cdb4aa9239c6e2f23e3977307
pakata goh <pakatagohlh@gmail.com>
2022-02-24 17:40:08 +0800

    feat: new codility solutions

---
+++ javascript/others/mergeSortAlgo/index.js
@@ -0,0 +1,34 @@
+const arr = [-1, 6, 3, 4, 7, 4];
+
+let count = 0;
+
+function merge(left, right) {
+  let sortedArr = []; // the sorted elements will go here
+
+  while (left.length && right.length) {
+    // insert the smallest element to the sortedArr
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+
+  return [...sortedArr, ...left, ...right];
+}
+
+function mergeSort(arr) {
+  const half = arr.length / 2;
+
+  // the base case is array length <=1
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const left = arr.splice(0, half); // the first half of the array
+  const right = arr;
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+console.log(mergeSort(arr));
+console.log(count);
